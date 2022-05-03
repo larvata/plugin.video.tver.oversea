12318bcb639e69026039416e9820affcc5da506c
Larvata <alzzlnewtype@gmail.com>
2022-05-04 03:18:00 +0800

    upgrade to tver api v2

--- src/main.py
+++ src/main.py
@@ -22,10 +22,9 @@ VIEW_WIDE_LIST = 55
 
 API_BASE = 'http://tver.larvata.me:5433/api'
 # API_BASE = 'http://localhost:5433/api'
-API_GENRES = f'{API_BASE}/genre?href={{}}'
+API_GENRES = f'{API_BASE}/genre/{{}}'
 API_LIST = f'{API_BASE}/list/{{}}'
-API_PLAYLIST = f'{API_BASE}/playlist/{{}}/{{}}'
-
+API_PLAYLIST = f'{API_BASE}/playlist/{{}}'
 
 # plugin url
 _URL = sys.argv[0]
@@ -39,51 +38,42 @@ def get_url(**kwargs):
     return f'{_URL}?{urlencode(kwargs)}'
 
 
-def render(parent_title='', genre_href=''):
-    resp_json = fetch_json(url=API_GENRES.format(genre_href))
+def render(parent_title='', genre_id=''):
+    print(API_GENRES.format(genre_id))
+    resp_json = fetch_json(url=API_GENRES.format(genre_id))
     xbmcplugin.setPluginCategory(_handle, parent_title)
     xbmcplugin.setContent(_handle, 'videos')
 
     is_list = True
-    for genre in resp_json:
-        title = genre.get('title')
-        href = genre.get('href')
-        genre_type = genre.get('type')
+    for result in resp_json:
+        title = result.get('name')
+        genre_id = result.get('id')
+        result_type = result.get('type')
         list_item = xbmcgui.ListItem(label=title)
 
         url = None
-        if genre_type == 'catchup':
+        if result_type == 'episode':
             is_list = False
-            pub = genre.get('publisher_id')
-            ref = genre.get('reference_id')
-            title = genre.get('title')
-            subtitle = genre.get('subtitle')
-            media = genre.get('media')
-            expire = genre.get('expire')
-            poster_url = genre.get('images')[0].get('image')
-            date = genre.get('date')
-            url = API_PLAYLIST.format(pub, ref)
-            list_item = xbmcgui.ListItem(label=f'{title}')
+            episode_id = result.get('id')
+            title = result.get('seriesTitle')
+            thumbnail = result.get('thumbnail')
+            details = result.get('details')
+            url = API_PLAYLIST.format(episode_id)
+            list_item = xbmcgui.ListItem(label=title)
             list_item.setArt({
-                'poster': poster_url,
+                'poster': thumbnail,
             })
             list_item.setInfo('video', {
-                'plot': f'{date}\n{expire}\n{media}\n\n{subtitle}'
+                'plot': details
             })
             list_item.setProperty('inputstream', 'inputstream.adaptive')
             list_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
-        elif genre_type == 'special':
-            url = get_url(genre_href=href, parent_title=title)
+            xbmc.executebuiltin(f'Container.SetViewMode({VIEW_INFO_WALL})')
         else:
-            # toplevel
-            url = get_url(genre_href=href, parent_title=title)
-        # elif genre_type == 'section':
+            # top level
+            url = get_url(genre_id=genre_id, parent_title=title)
+            xbmc.executebuiltin(f'Container.SetViewMode({VIEW_WIDE_LIST})')
         xbmcplugin.addDirectoryItem(_handle, url, list_item, is_list)
-
-    if is_list:
-        xbmc.executebuiltin(f'Container.SetViewMode({VIEW_WIDE_LIST})')
-    else:
-        xbmc.executebuiltin(f'Container.SetViewMode({VIEW_INFO_WALL})')
     xbmcplugin.endOfDirectory(_handle)
 
 
@@ -92,7 +82,7 @@ def router(paramstring):
     TODO doc
     """
     params = dict(parse_qsl(paramstring))
-    render(genre_href=params.get('genre_href', ''), parent_title=params.get('parent_title'))
+    render(genre_id=params.get('genre_id', ''), parent_title=params.get('parent_title'))
 
 
 if __name__ == '__main__':
